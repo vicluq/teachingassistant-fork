83e76b7b300ef3c8a3699da1f32b016e3c4fce71
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:26:56 -0300

    merged commit

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -4,13 +4,16 @@ export class AlunoService {
   alunos: Aluno[] = [];
   
   gravar(aluno: Aluno): Aluno | null {
-    let result: Aluno | null = null;
+    let result = null;
     
-    if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
 
     return result;
+  }
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
   }
 }
