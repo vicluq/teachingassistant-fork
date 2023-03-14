e1b8838dfacaf0839f0fce1c740f72c56c60eca5
vicluq <victoria.tewari@gmail.com>
2023-03-14 13:19:35 -0300

    added github login property to student

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -6,13 +6,19 @@ import { NgModule } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  aluno: Aluno = { nome: '', cpf: '', email: '' };
+  aluno: Aluno = { nome: '', cpf: '', email: '', github_login: '' };
 }
 export class Aluno {
+  constructor(nome: string, cpf: string, email: string, github_login: string) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.github_login = github_login;
+  }
+
   nome: string;
   cpf: string;
   email: string;
+  github_login: string;
 }
-
