d556a76a63c5d43a2ca6b627b7282786f031bf70
vicluq <victoria.tewari@gmail.com>
2023-03-15 11:44:27 -0300

    merged commit

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -3,12 +3,14 @@ import { Aluno } from './aluno';
 export class AlunoService {
   alunos: Aluno[] = [];
   
-  gravar(aluno: Aluno): Aluno {
-    var result = null;
+  gravar(aluno: Aluno): Aluno | null {
+    let result: Aluno | null = null;
+    
     if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
+
     return result;
   }
 }
